37fb90fa57e29675d59f6ca9dac4c3748547dd52
dandy <1055671428@qq.com>
2019-05-30 20:35:32 +0800

    session,cookie

--- server/index.js
+++ server/index.js
@@ -1,23 +1,36 @@
 const Koa = require('koa')
 const app = new Koa()
 const logger = require('koa-logger')
+const sessino = require('koa-session')
 // 测试中间件
-const mid1 = async (ctx, next) => {
-    ctx.body = 'Hi'
-    await next()
-    ctx.body += ' there'
-}
-const mid2 = async (ctx, next) => {
-    ctx.type = 'text/html; chartset=utf-8'
-    await next()
-}
-const mid3 = async (ctx, next) => {
-    ctx.body += ' koa'
-    await next()
-}
+// const mid1 = async (ctx, next) => {
+//     ctx.body = 'Hi'
+//     await next()
+//     ctx.body += ' there'
+// }
+// const mid2 = async (ctx, next) => {
+//     ctx.type = 'text/html; chartset=utf-8'
+//     await next()
+// }
+// const mid3 = async (ctx, next) => {
+//     ctx.body += ' koa'
+//     await next()
+// }
 // use的顺序就是中间件执行的顺序，等执行完，则将控制权将给上层中间件
+app.keys = ['hi koa']
 app.use(logger())
-app.use(mid1)
-app.use(mid2)
-app.use(mid3)
+app.use(sessino(app))
+app.use(async (ctx, next) =>{
+    if (ctx.path === '/favicon.ico') return
+    if (ctx.path === '/') {
+        let n = ctx.session.view || 0
+        ctx.session.view = ++n
+        ctx.body = `${n}次`
+    } else if (ctx.path === '/hi') {
+        ctx.body = 'hi koa'
+    } else {
+        ctx.body = '404'
+    }
+    await next()
+})
 app.listen(3333)
